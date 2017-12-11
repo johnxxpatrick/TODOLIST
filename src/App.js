2963b08bd321733d195b92744da507cca16d378f
John Patrick <johnxxpatrickk@gmail.com>
2017-12-11 15:49:28 +0800

    add

--- src/App.js
+++ src/App.js
@@ -5,83 +5,103 @@ import './App.css';
 
 class App extends Component {
 
-  state = {
-      todo_empty: '',
-      todos: [],
-      id : 1 ,
-      status : '',
-      item_left : 0,
-      input : '',
-      completed: 0
+  state =
+    {
+        todo_empty: '',
+        todos: [],
+        id : 1 ,
+        status : '',
+        item_left : 0,
+
     }
 
-  handleChange(prop, e) {
-    this.setState({ [prop]: e.target.value })
-    this.removeTodo = this.removeTodo.bind(this)
-    this.setState({input: e.target.value})
+  handleChange(prop, e)
+    {
+      this.setState({ [prop]: e.target.value })
+      this.removeTodo = this.removeTodo.bind(this)
+      this.setState({input: e.target.value})
     }
 
-  handleSave(e) {
+  handleSave(e)
+    {
+      var obj =
+      {
+        status : false,
+        id: this.state.id,
+        todos : this.state.todo_empty,
+        item_left : this.state.item_left,
 
-    var obj = {
-      status : false,
-      id: this.state.id,
-      todos : this.state.todo_empty,
-      item_left : this.state.item_left,
-      completed : this.state.completed
+      }
+      this.setState
+      ({
+        todos : this.state.todos.concat(obj),
+        input : '',
+        id : this.state.id + 1,
+        item_left : this.state.item_left + 1
+      })
+
     }
-    this.setState ({
-      todos : this.state.todos.concat(obj),
-      input : '',
-      id : this.state.id + 1,
-      item_left : this.state.item_left + 1
+
+  removeTodo(item)
+    {
+      this.setState({
+
+      item_left:this.state.todos.length - 1
     })
-  }
-
-  removeTodo(item){
-    this.setState({
-      item_left: this.state.item_left - 1,
-      completed: this.state.completed - 1
-    })
-    const newState = this.state.todos
-    	if (newState.indexOf(item) > -1) {
-      	newState.splice(newState.indexOf(item), 1)
+        const newState = this.state.todos
+      	if (newState.indexOf(item) > -1)
+      {
+    	  newState.splice(newState.indexOf(item), 1)
         this.setState({todos: newState})
-  }
-}
-  handleComplete(data){
-    this.setState(prevState => {
-    return {completed: data == 'add' ? prevState.completed - 1: prevState.completed + 1}
-    })
+      }
+    }
 
-    var newList = this.state.todos
-    .map(item => {
-     const isSelected = item.id  === data.id
-     if(isSelected) item.status = !item.status
+  handleComplete(data)
+  {
+        var newList = this.state.todos
+        .map(item =>
+      {
+        const isSelected = item.id  === data.id
+        if(isSelected) item.status = !item.status
+        return item
+      })
+        var totalCompleted = this.state.todos.reduce(function(complete, incomplete)
+        {
+        var addComplete = incomplete.status ? 1 : 0
+        complete += addComplete
+        return complete
+        }, 0)
+        this.setState({
+        totalCompleted:this.state.totalCompleted,
+        item_left:this.state.todos.length - totalCompleted,
 
-     return item
+        })
 
-  })
-}
-
+   }
 
   render() {
 
-    console.log ('Todos:', this.state.todos)
+    var totalCompleted = this.state.todos.reduce(function(complete, incomplete)
+    {
+    var addComplete = incomplete.status ? 1 : 0
+    complete += addComplete
+    return complete
+    }, 0)
 
     return (
           <div className="App">
               <header className="App-header">
+
               </header>
-            <TodoForm
+              <TodoForm
+                  total={totalCompleted}
                   handleChange={this.handleChange.bind(this,'todo_empty')}
                   handleSave={this.handleSave.bind(this)}
                   removeitem_left={this.state.item_left}
                   removeTodo={this.removeTodo}
                   handleComplete={this.handleComplete.bind(this)}
-
                   {...this.state}
-                />
+              />
          </div>
     );
   }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -9,33 +9,42 @@ class TodoForm extends Component {
 
 
   render() {
-      const { todos , item_left, completed  } = this.props
+      const { todos , item_left, total  } = this.props
       const todolist = todos
-      console.log ('comments', todolist)
+      console.log('todoCompleted:', total)
+      console.log('todoCompleted:', this.props)
+
     return (
         <center>
         <h1>TODO LIST</h1>
           <div className="field">
               <input type="text" placeholder="What needs to be done ?"
-               name="message" rows="3" cols="40" className="note"
-               onChange={this.props.handleChange}
-               value={this.props.input}/>
+                  name="message" rows="3" cols="40" className="note"
+                  onChange={this.props.handleChange}
+                  value={this.props.input}/>
               <button type="button" className="save"
-               onClick={this.props.handleSave} disabled={!this.props.input} >ADD
+                  onClick={this.props.handleSave} disabled={!this.props.input}>ADD
               </button>
           </div>
-          <div className="item-left">{item_left} Items left</div>
-          <div className="item-left">{completed} Items completed</div>
+          <table className="item-left">
+          <tr>
+          <th>{item_left} Items Left</th>
+          <th>{total} Items Completed</th>
+          </tr>
+          </table>
+
             {
               todolist.map((item, index) => {
                 return <ul id="myUL">
                           <li key={index}>
-                            <input type="checkbox" id="cbox3"
-                              onClick={this.props.handleComplete.bind(this, item)} />
-                                <label forHTML="cbox3">{item.todos}
-                                </label>
-                                <button className="btnRemove" onClick={this.props.removeTodo.bind(this, item)}>REMOVE
-                                </button>
+                            <input type="checkbox" checked={item.status}
+                                onClick={this.props.handleComplete.bind(this, item)} />
+                            <label style={{ textDecoration: item.status ? 'line-through' : 'none',
+                             color: item.status ? 'red' : 'black'}}>{item.todos}
+                            </label>
+                            <button className="btnRemove"
+                                onClick={this.props.removeTodo.bind(this, item)}>REMOVE
+                            </button>
                           </li>
                       </ul>
               })
